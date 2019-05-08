003ece14e31328201414fb6a15a4adc09eb90827
dalmariko <dalmarikocamp@gmail.com>
2019-05-08 09:10:57 +0300

    deploing settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCrfzEuISQO2vSHkF7mJaa1hdfn6_9xz_0',
+    authDomain: 'angularfiebase-authentic-7d417.firebaseapp.com',
+    databaseURL: 'https://angularfiebase-authentic-7d417.firebaseio.com',
+    projectId: 'angularfiebase-authentic-7d417',
+    storageBucket: 'angularfiebase-authentic-7d417.appspot.com',
+    messagingSenderId: '99344476457',
+    appId: '1:99344476457:web:dd29929e36edf7a0'
+  }
 };
