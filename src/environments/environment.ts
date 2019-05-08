500be2dd2e0ee2c9a71bb82e61f7970e4fc7c2c4
dalmariko <dalmarikocamp@gmail.com>
2019-05-08 06:33:49 +0300

    all logical complite

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCrfzEuISQO2vSHkF7mJaa1hdfn6_9xz_0',
+    authDomain: 'angularfiebase-authentic-7d417.firebaseapp.com',
+    databaseURL: 'https://angularfiebase-authentic-7d417.firebaseio.com',
+    projectId: 'angularfiebase-authentic-7d417',
+    storageBucket: 'angularfiebase-authentic-7d417.appspot.com',
+    messagingSenderId: '99344476457',
+    appId: '1:99344476457:web:dd29929e36edf7a0'
+  }
 };
 
 /*
